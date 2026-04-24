589acdf8739de8fcff1a1d1fa4dc633d894f769b
MoryZang <moryzang@example.com>
2026-04-24 22:24:16 +0800

    数据库保存

--- src/main/java/com/old/silence/mq/center/domain/repository/PermissionAuditLogRepository.java
+++ src/main/java/com/old/silence/mq/center/domain/repository/PermissionAuditLogRepository.java
@@ -1,6 +1,7 @@
 package com.old.silence.mq.center.domain.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.old.silence.data.mybatis.projection.ProjectionMapperRepository;
 import com.old.silence.mq.center.domain.model.PermissionAuditLog;
@@ -11,8 +12,10 @@ import java.util.List;
 /**
  * 权限审计日志 Repository
  */
+@Mapper
 public interface PermissionAuditLogRepository extends ProjectionMapperRepository<PermissionAuditLog, BigInteger> {
 
+    @Select("SELECT * FROM rmq_permission_audit_log WHERE operation_result = false")
     List<PermissionAuditLog> findBySuccessFalse();
 
 }
