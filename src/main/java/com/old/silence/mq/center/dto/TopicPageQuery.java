72e0d98a150e0acc93fd3bd2c7fdcff0a2d0fc96
MoryZang <moryzang@example.com>
2026-06-01 17:03:03 +0800

    111

--- src/main/java/com/old/silence/mq/center/dto/TopicPageQuery.java
+++ src/main/java/com/old/silence/mq/center/dto/TopicPageQuery.java
@@ -3,18 +3,14 @@ package com.old.silence.mq.center.dto;
 import com.old.silence.data.commons.annotation.RelationalQueryProperty;
 import com.old.silence.data.commons.converter.Part;
 
-import java.math.BigInteger;
 
 public class TopicPageQuery {
 
     private String keyword;
 
-
     @RelationalQueryProperty(type = Part.Type.SIMPLE_PROPERTY)
     private String status;
 
-    @RelationalQueryProperty(type = Part.Type.SIMPLE_PROPERTY)
-    private BigInteger ownerId;
 
     @RelationalQueryProperty(type = Part.Type.SIMPLE_PROPERTY)
     private Boolean systemTopic;
@@ -33,14 +29,6 @@ public class TopicPageQuery {
 
     public void setStatus(String status) {
         this.status = status;
-    }
-
-    public BigInteger getOwnerId() {
-        return ownerId;
-    }
-
-    public void setOwnerId(BigInteger ownerId) {
-        this.ownerId = ownerId;
     }
 
     public Boolean getSystemTopic() {
